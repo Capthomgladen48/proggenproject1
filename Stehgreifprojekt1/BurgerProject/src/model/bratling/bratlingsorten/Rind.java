7f2798126a8fa5b557d56d7b41cd7f787a32c70e
Capthomgladen48 <38947045+Capthomgladen48@users.noreply.github.com>
2018-05-15 17:43:44 +0200

    Update Rind.java

--- Stehgreifprojekt1/BurgerProject/src/model/bratling/bratlingsorten/Rind.java
+++ Stehgreifprojekt1/BurgerProject/src/model/bratling/bratlingsorten/Rind.java
@@ -2,9 +2,12 @@ package model.bratling.bratlingsorten;
 import model.bratling.Bratling;
 
 public abstract class Rind extends Bratling {
-        public Rind () {
-            super("Rindfleisch", 20, 1.85, true, false, false);
-
-        }
+    public Rind () {
+        super("Rindfleisch", 20, 1.85, true, false, false);
+            bratzeit = 270;
+            hoehe = 2.5;
     }
-
+    public void setHoehe (double bratlingsortenHoehe){
+        hoehe=bratlingsortenHoehe;
+    }
+}
