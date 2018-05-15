e2c15ae520a9b05562b10485c653e2b247a30e0a
Capthomgladen48 <38947045+Capthomgladen48@users.noreply.github.com>
2018-05-15 17:55:52 +0200

    Update Sauce.java

--- Stehgreifprojekt1/BurgerProject/src/model/saucen/Sauce.java
+++ Stehgreifprojekt1/BurgerProject/src/model/saucen/Sauce.java
@@ -3,24 +3,24 @@ import model.Zutat;
 
 public class Sauce extends Zutat {
 
-	private int menge;
-	private String geschmack;
-	
-	public Sauce (String name, int nummer, double preis, boolean klassisch, boolean vegan, boolean vegetarisch) {
-		super (name, nummer, preis, klassisch, vegan, vegetarisch);
-		this.menge = menge;
-		this.geschmack = geschmack;
-	}
-	
-	@Override
-	public int zubereiten () {
+    public int menge; //in Milliliter
+    public String geschmack;
 
-		return 0;
-	}
+    public Sauce (String name, int nummer, double preis, boolean klassisch, boolean vegan, boolean vegetarisch) {
+        super (name, nummer, preis, klassisch, vegan, vegetarisch);
+        this.menge = menge;
+        this.geschmack = geschmack;
+    }
 
-	@Override
-	public int berechneHoehe() {
+    @Override
+    public int zubereiten () {
 
-		return 0;
-	}
+        return 0;
+    }
+
+    @Override
+    public double berechneHoehe() {
+
+        return 0;
+    }
 }
