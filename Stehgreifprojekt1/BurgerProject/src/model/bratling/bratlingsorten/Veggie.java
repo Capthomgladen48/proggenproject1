f231d05d4e7c15fd7677852cc488992b1ec02cdf
Capthomgladen48 <38947045+Capthomgladen48@users.noreply.github.com>
2018-05-15 17:44:19 +0200

    Update Veggie.java

--- Stehgreifprojekt1/BurgerProject/src/model/bratling/bratlingsorten/Veggie.java
+++ Stehgreifprojekt1/BurgerProject/src/model/bratling/bratlingsorten/Veggie.java
@@ -5,5 +5,11 @@ public abstract class Veggie extends Bratling {
 
     public Veggie() {
         super("Veggie", 21, 1.75, false, false, true);
+            bratzeit = 240;
+            hoehe = 2.5;
+    }
+    public void setHoehe (double bratlingsortenHoehe){
+        
+        hoehe=bratlingsortenHoehe;
     }
 }
