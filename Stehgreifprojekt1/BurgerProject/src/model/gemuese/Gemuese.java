5f62eda03b65ffe2cad68be30331a8ce439cec6f
Capthomgladen48 <38947045+Capthomgladen48@users.noreply.github.com>
2018-05-15 17:51:45 +0200

    Update Gemuese.java

--- Stehgreifprojekt1/BurgerProject/src/model/gemuese/Gemuese.java
+++ Stehgreifprojekt1/BurgerProject/src/model/gemuese/Gemuese.java
@@ -3,24 +3,29 @@ import model.Zutat;
 
 public abstract class Gemuese extends Zutat {
 
-	private int scheibenDicke;
-	private int scheibenAnzahl;
-	
-	public Gemuese (String name, int nummer, double  preis, boolean klassisch) {
-		super(name, nummer, preis, klassisch, true, true);
-		this.scheibenAnzahl = scheibenAnzahl;
-		this.scheibenDicke = scheibenDicke;
-	}
-	
-	public int zubereiten () {
-		int zubereitungszeit = scheibenAnzahl*1;
-		return zubereitungszeit;
-	}
-	
-	public int berechneHoehe () {
-		return 0;	
-	}
-	
+    public double scheibenDicke; //in cm
+    protected int scheibenAnzahl; //Anzahl der Scheiben/Ringe
+    protected double gemuesesortenHoehe;
+
+
+    public Gemuese (String name, int nummer, double  preis, boolean klassisch) {
+        super(name, nummer, preis, klassisch, true, true);
+        this.scheibenAnzahl = scheibenAnzahl;
+        this.scheibenDicke = scheibenDicke;
+    }
+
+    public int zubereiten () {
+        int zubereitungszeit = scheibenAnzahl*1;
+        return zubereitungszeit;
+    }
+
+    public double berechneHoehe () {
+        return gemuesesortenHoehe;
+    }
+    public double getgemuesesortenHoehe() {
+        return gemuesesortenHoehe;
+    }
+
 //	public String toString () {
 //		return String;
 //	}
