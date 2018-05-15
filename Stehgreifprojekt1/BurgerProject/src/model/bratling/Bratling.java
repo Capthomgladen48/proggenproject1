886bfd7d4cfabfaef46cdcb6b00a64226234b2de
Capthomgladen48 <38947045+Capthomgladen48@users.noreply.github.com>
2018-05-15 20:14:05 +0200

    Update Bratling.java

--- Stehgreifprojekt1/BurgerProject/src/model/bratling/Bratling.java
+++ Stehgreifprojekt1/BurgerProject/src/model/bratling/Bratling.java
@@ -3,15 +3,24 @@ import model.Zutat;
 
 public abstract class Bratling extends Zutat {
 
-    private int bratzeit;
-    private int hoehe;
+    public int bratzeit; //in Sekunden
+    protected double hoehe; //in cm
+    protected double bratlingsortenHoehe;
 
-    public Bratling (String name, int nummer, double preis, boolean klassisch, boolean vegan, boolean vegetarisch) {
+    public Bratling(String name, int nummer, double preis, boolean klassisch, boolean vegan, boolean vegetarisch) {
         super(name, nummer, preis, klassisch, vegan, vegetarisch);
         this.bratzeit = bratzeit;
         this.hoehe = hoehe;
     }
+    public int zubereiten () {
 
+        return 0;
+    }
 
-
+    public double berechneHoehe () {
+       return bratlingsortenHoehe;
+    }
+    public double getbratlingsortenHoehe() {
+        return bratlingsortenHoehe;
+    }
 }
