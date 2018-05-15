5bc7c53be94d79c06bc01dc26ff038b02d64a60b
Capthomgladen48 <38947045+Capthomgladen48@users.noreply.github.com>
2018-05-15 17:46:17 +0200

    Update Ciabatta.java

--- Stehgreifprojekt1/BurgerProject/src/model/broetchen/broetchensorten/Ciabatta.java
+++ Stehgreifprojekt1/BurgerProject/src/model/broetchen/broetchensorten/Ciabatta.java
@@ -4,5 +4,10 @@ import model.broetchen.Broetchen;
 public class Ciabatta extends Broetchen {
     public Ciabatta () {
         super("Ciabatta", 13, 0.45, false, false, true);
+            backzeit = 330;
+            hoehe = 4.1;
+    }
+    public void setHoehe (double broetchensortenHoehe){
+        hoehe=broetchensortenHoehe;
     }
 }
