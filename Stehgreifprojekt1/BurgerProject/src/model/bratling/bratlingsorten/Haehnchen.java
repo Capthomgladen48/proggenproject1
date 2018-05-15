4a7ef1d98399ae848310fb68477269212d75a4c9
Capthomgladen48 <38947045+Capthomgladen48@users.noreply.github.com>
2018-05-15 17:42:58 +0200

    Update Haehnchen.java

--- Stehgreifprojekt1/BurgerProject/src/model/bratling/bratlingsorten/Haehnchen.java
+++ Stehgreifprojekt1/BurgerProject/src/model/bratling/bratlingsorten/Haehnchen.java
@@ -4,5 +4,10 @@ import model.bratling.Bratling;
 public abstract class Haehnchen extends Bratling {
     public Haehnchen () {
         super("Haehnchen", 21, 1.15, false, false, false);
+            bratzeit = 180;
+            hoehe = 1.1;
+    }
+    public void setHoehe (double bratlingsortenHoehe){
+        hoehe=bratlingsortenHoehe;
     }
 }
