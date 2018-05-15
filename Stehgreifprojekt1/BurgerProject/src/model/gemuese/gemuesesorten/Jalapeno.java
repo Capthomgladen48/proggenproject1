2255e539a1938503a478a689b2e26c465a3e3a9b
Capthomgladen48 <38947045+Capthomgladen48@users.noreply.github.com>
2018-05-15 20:09:20 +0200

    Update Jalapeno.java

--- Stehgreifprojekt1/BurgerProject/src/model/gemuese/gemuesesorten/Jalapeno.java
+++ Stehgreifprojekt1/BurgerProject/src/model/gemuese/gemuesesorten/Jalapeno.java
@@ -5,5 +5,11 @@ public abstract class Jalapeno extends Gemuese {
 
     public Jalapeno () {
         super("Jalapeno-Ringe", 43, 0.08, false);
+            scheibenAnzahl = 5;
+            scheibenDicke = 0.2;
+    }
+    public void setscheibenDicke (double gemuesesortenHoehe){
+        scheibenDicke=gemuesesortenHoehe;
     }
 }
+
