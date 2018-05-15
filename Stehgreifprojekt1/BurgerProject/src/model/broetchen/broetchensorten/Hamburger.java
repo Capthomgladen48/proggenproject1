26849020692b67a58ffae2d4062e3b6a1ea4d925
Capthomgladen48 <38947045+Capthomgladen48@users.noreply.github.com>
2018-05-15 17:47:26 +0200

    Update Hamburger.java

--- Stehgreifprojekt1/BurgerProject/src/model/broetchen/broetchensorten/Hamburger.java
+++ Stehgreifprojekt1/BurgerProject/src/model/broetchen/broetchensorten/Hamburger.java
@@ -5,6 +5,11 @@ public class Hamburger extends Broetchen {
 
     public Hamburger () {
         super("Hamburger", 10, 0.85, true, false, true);
+            backzeit = 90;
+            hoehe = 2.7;
+    }
+    public void setHoehe (double broetchensortenHoehe){
+        hoehe=broetchensortenHoehe;
     }
 
 }
