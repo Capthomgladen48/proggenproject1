42806868311dbf7b17835dc0d1c0f2cd2baf0b25
Capthomgladen48 <38947045+Capthomgladen48@users.noreply.github.com>
2018-05-15 17:47:56 +0200

    Update VeganBroetchen.java

--- Stehgreifprojekt1/BurgerProject/src/model/broetchen/broetchensorten/VeganBroetchen.java
+++ Stehgreifprojekt1/BurgerProject/src/model/broetchen/broetchensorten/VeganBroetchen.java
@@ -5,5 +5,10 @@ public class VeganBroetchen extends Broetchen {
 
     public VeganBroetchen () {
         super("Veganes Brötchen", 12, 0.95, false, true, true);
+            backzeit = 240;
+            hoehe = 3.4;
+    }
+    public void setHoehe (double broetchensortenHoehe){
+        hoehe=broetchensortenHoehe;
     }
 }
