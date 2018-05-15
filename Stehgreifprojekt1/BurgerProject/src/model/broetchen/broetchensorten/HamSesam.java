19f23c9c46165d7adfa2b8abe6349284b4732379
Capthomgladen48 <38947045+Capthomgladen48@users.noreply.github.com>
2018-05-15 17:46:47 +0200

    Update HamSesam.java

--- Stehgreifprojekt1/BurgerProject/src/model/broetchen/broetchensorten/HamSesam.java
+++ Stehgreifprojekt1/BurgerProject/src/model/broetchen/broetchensorten/HamSesam.java
@@ -5,5 +5,10 @@ public class HamSesam extends Broetchen {
 
     public HamSesam () {
         super("Hamburger Sesam", 11, 0.95, true, false, true);
+            backzeit = 90;
+            hoehe = 2.8;
+    }
+    public void setHoehe (double broetchensortenHoehe){
+        hoehe=broetchensortenHoehe;
     }
 }
