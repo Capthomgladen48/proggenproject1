3322645ee7336601c69fe53bcd885e9d44aa1df2
Capthomgladen48 <38947045+Capthomgladen48@users.noreply.github.com>
2018-05-15 17:50:38 +0200

    Update Tomate.java

--- Stehgreifprojekt1/BurgerProject/src/model/gemuese/gemuesesorten/Tomate.java
+++ Stehgreifprojekt1/BurgerProject/src/model/gemuese/gemuesesorten/Tomate.java
@@ -5,5 +5,11 @@ public class Tomate extends Gemuese {
 
     public Tomate () {
         super("Tomate", 40, 0.25, true);
+            scheibenAnzahl = 3;
+            scheibenDicke = 0.3;
+    }
+    public void setscheibenDicke (double gemuesesortenHoehe){
+        
+        scheibenDicke=gemuesesortenHoehe;
     }
 }
