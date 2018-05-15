601eef7452dbbc0d4aac93d26dbc5572c0ebaea6
Capthomgladen48 <38947045+Capthomgladen48@users.noreply.github.com>
2018-05-15 17:45:31 +0200

    Update Broetchen.java

--- Stehgreifprojekt1/BurgerProject/src/model/broetchen/Broetchen.java
+++ Stehgreifprojekt1/BurgerProject/src/model/broetchen/Broetchen.java
@@ -3,20 +3,26 @@ import model.Zutat;
 
 public abstract class Broetchen extends Zutat {
 
-	private int backzeit;
-	private int hoehe;
-	
-	public Broetchen (String name, int nummer, double preis, boolean klassisch, boolean vegan, boolean vegetarisch) {
-		super (name, nummer, preis, klassisch, vegan, vegetarisch);
-		this.backzeit = backzeit;
-		this.hoehe = hoehe;
-	}
-	
-	public int zubereiten () {
-		return 0;
-	}
+    public int backzeit; //in Sekunden
+    protected double hoehe; //in cm
+    protected double broetchensortenHoehe;
 
-	public int berechneHoehe () {
-		return 0;
-	}
+
+    public Broetchen (String name, int nummer, double preis, boolean klassisch, boolean vegan, boolean vegetarisch) {
+        super (name, nummer, preis, klassisch, vegan, vegetarisch);
+        this.backzeit = backzeit;
+        this.hoehe = hoehe;
+    }
+
+    public int zubereiten () {
+
+        return 0;
+    }
+
+    public double berechneHoehe () {
+        return broetchensortenHoehe;
+    }
+    public double getbroetchensortenHoehe() {
+        return broetchensortenHoehe;
+    }
 }
