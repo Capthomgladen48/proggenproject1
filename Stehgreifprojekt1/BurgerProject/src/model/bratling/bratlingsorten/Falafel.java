ef23567b33bf73c54001e254847447a2b924f45c
Capthomgladen48 <38947045+Capthomgladen48@users.noreply.github.com>
2018-05-15 17:41:28 +0200

    Update Falafel.java

--- Stehgreifprojekt1/BurgerProject/src/model/bratling/bratlingsorten/Falafel.java
+++ Stehgreifprojekt1/BurgerProject/src/model/bratling/bratlingsorten/Falafel.java
@@ -5,6 +5,10 @@ public abstract class Falafel extends Bratling {
 
     public Falafel () {
         super("Falafel", 22 , 1.45, false, true, true);
-
-        }
+            bratzeit = 210;
+            hoehe = 2.1;
     }
+    public void setHoehe (double bratlingsortenHoehe){
+        hoehe=bratlingsortenHoehe;
+    }
+}
