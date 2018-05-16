74320af4a31c75d99fd9be530a450d9c2c6d0700
Capthomgladen48 <38947045+Capthomgladen48@users.noreply.github.com>
2018-05-16 17:59:39 +0200

    Update Zutat.java

--- Stehgreifprojekt1/BurgerProject/src/model/Zutat.java
+++ Stehgreifprojekt1/BurgerProject/src/model/Zutat.java
@@ -1,26 +1,38 @@
-package model;
+ackage model;
 
 public abstract class Zutat {
 
-	private String name;
-	private int nummer;
-	private double  preis;
-	private boolean klassisch;
-	private boolean vegan;
-	private boolean vegetarisch;
+    protected String name;
+    protected int nummer;
+    protected double  preis;
+    protected boolean klassisch;
+    protected boolean vegan;
+    protected boolean vegetarisch;
+    protected double burgerHoehe;
+    protected double broetchenHoehe;
+    protected double bratlingHoehe;
+    protected double scheibenDicke;
 
-	public Zutat (String name, int nummer, double preis, boolean klassisch, boolean vegan, boolean vegetarisch) {
-		this.name = name;
-		this.nummer = nummer;
-		this.preis = preis;
-		this.klassisch = klassisch;
-		this.vegan = vegan;
-		this.vegetarisch = vegetarisch;		
-	}
-	
-	public abstract int zubereiten ();
 
-	
-	public abstract int berechneHoehe();
 
+    public Zutat (String name, int nummer, double preis, boolean klassisch, boolean vegan, boolean vegetarisch) {
+        this.name = name;
+        this.nummer = nummer;
+        this.preis = preis;
+        this.klassisch = klassisch;
+        this.vegan = vegan;
+        this.vegetarisch = vegetarisch;
+    }
+
+    public abstract int zubereiten ();
+
+
+    public double berechneHoehe (){
+        burgerHoehe = broetchenHoehe + bratlingHoehe + scheibenDicke;
+        return burgerHoehe;
+    }
+
+    public double getPreis() {
+        return preis;
+    }
 }
