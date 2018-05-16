d978e18dec8930930438a157076d7df2381a2aa1
Capthomgladen48 <38947045+Capthomgladen48@users.noreply.github.com>
2018-05-16 19:17:43 +0200

    Update Bratling.java

--- Stehgreifprojekt1/BurgerProject/src/model/bratling/Bratling.java
+++ Stehgreifprojekt1/BurgerProject/src/model/bratling/Bratling.java
@@ -3,15 +3,32 @@ import model.Zutat;
 
 public abstract class Bratling extends Zutat {
 
-    private int bratzeit;
-    private int hoehe;
+    public int bratzeit; //in Sekunden
+    protected double hoehe; //in cm
 
-    public Bratling (String name, int nummer, double preis, boolean klassisch, boolean vegan, boolean vegetarisch) {
+
+    public Bratling(String name, int nummer, double preis, boolean klassisch, boolean vegan, boolean vegetarisch) {
         super(name, nummer, preis, klassisch, vegan, vegetarisch);
         this.bratzeit = bratzeit;
         this.hoehe = hoehe;
     }
+    public int zubereiten () {
+        if (nummer == 20){
+            System.out.println("Das Rindfleischpatty wird gebraten und ist in vier einhalb Minuten fertig.");
+        } else if (nummer == 21){
+            System.out.println("Das gegrillte Hähnchenfleisch wird gebraten und ist in drei Minuten fertig.");
+        }else if (nummer == 22){
+            System.out.println("Der Falafelbratling wird gebraten und ist in drei einhalb Minuten fertig.");
+        }else if (nummer == 23)
+            System.out.println("Der Gemüse Bratling wird geraten und ist in vier Minuten fertig.");
+
+        return bratzeit;
+    }
 
 
+    public double berechneHoehe () {
+        double bratlingHoehe = (hoehe * 0.965) * bratzeit / 60;
+        return bratlingHoehe;
+    }
 
 }
