4986b3900f624f076d60483260adbb48d0abd4ac
Capthomgladen48 <38947045+Capthomgladen48@users.noreply.github.com>
2018-05-16 17:46:05 +0200

    Update Broetchen.java

--- Stehgreifprojekt1/BurgerProject/src/model/broetchen/Broetchen.java
+++ Stehgreifprojekt1/BurgerProject/src/model/broetchen/Broetchen.java
@@ -12,11 +12,21 @@ public abstract class Broetchen extends Zutat {
 		this.hoehe = hoehe;
 	}
 	
-	public int zubereiten () {
-		return 0;
-	}
+	 public int zubereiten () {
+        if (nummer == 10){
+            System.out.println("Das Hamburgerbrötchen wird gebacken und benötigt anderthalb Minuten bis es goldbraun ist.");
+        } else if (nummer == 11){
+            System.out.println("Das Hamburgerbrötchen mit Sesamkörnern wird gebacken und benötigt Minuten bis es goldbraun ist.");
+        }else if (nummer == 12){
+            System.out.println("Das Vegane Brötchen wird gebacken und benötigt vier Minuten bis es goldbraun ist.");
+        }else if (nummer == 13)
+            System.out.println("Das Ciabatta Brötchen wird gebacken und benötigt fünf einhalb Minuten bis es goldbraun ist.");
 
-	public int berechneHoehe () {
-		return 0;
-	}
+        return backzeit;
+    }
+
+    public double berechneHoehe () {
+        double broetchenHoehe = (hoehe * 1.02) * backzeit / 60;
+        return broetchenHoehe;
+    }
 }
